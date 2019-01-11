4f0f4a800c3161b9386965274e361022d6560905
René Schultz Madsen <rm@microting.dk>
2019-01-11 12:01:36 +0100

    Changing the paths.

--- testinginstallpn.sh
+++ testinginstallpn.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-cd ../eform-angular-frontend/eform-client/src/app/plugins
-sed '/\/\/ INSERT ROUTES HERE/i {' plugins.routing.ts -i
-sed '/\/\/ INSERT ROUTES HERE/i path: "machine-area-pn",' plugins.routing.ts -i
-sed '/\/\/ INSERT ROUTES HERE/i canActivate: [AuthGuard],' plugins.routing.ts -i
-sed '/\/\/ INSERT ROUTES HERE/i loadChildren: "./modules/machine-area-pn/machine-area-pn.module#MachineAreaPnModule"' plugins.routing.ts -i
-sed '/\/\/ INSERT ROUTES HERE/i }' plugins.routing.ts -i
+sed '/\/\/ INSERT ROUTES HERE/i {' src/app/plugins/plugins.routing.ts -i
+sed '/\/\/ INSERT ROUTES HERE/i path: "machine-area-pn",' src/app/plugins/plugins.routing.ts -i
+sed '/\/\/ INSERT ROUTES HERE/i canActivate: [AuthGuard],' src/app/plugins/plugins.routing.ts -i
+sed '/\/\/ INSERT ROUTES HERE/i loadChildren: "./modules/machine-area-pn/machine-area-pn.module#MachineAreaPnModule"' src/app/plugins/plugins.routing.ts -i
+sed '/\/\/ INSERT ROUTES HERE/i }' src/app/plugins/plugins.routing.ts -i
 
