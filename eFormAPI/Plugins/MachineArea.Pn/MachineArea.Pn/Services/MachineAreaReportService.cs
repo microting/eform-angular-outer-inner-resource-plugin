3f67fa7d790b27e2c5dffd5d268ecfd23c7cd9fc
Gid <gid733@ya.ru>
2019-01-30 14:43:43 +0200

    Added simple linq expression

--- eFormAPI/Plugins/MachineArea.Pn/MachineArea.Pn/Services/MachineAreaReportService.cs
+++ eFormAPI/Plugins/MachineArea.Pn/MachineArea.Pn/Services/MachineAreaReportService.cs
@@ -34,7 +34,81 @@ namespace MachineArea.Pn.Services
         {
             try
             {
-
+                // var namesList = GetNamesList();
+                // var jobsList = GetJobs();
+                // 
+                // var type = 2; // 1- Month, 2- Week, 3- day
+                // 
+                // var dfi = DateTimeFormatInfo.CurrentInfo;
+                // 
+                // var reportEntitiesList = new List<ReportEntityModel>();
+                // 
+                // switch (type)
+                // {
+                //     case 1:
+                //         reportEntitiesList = jobsList.GroupBy(x => x.SiteId)
+                //             .Select(x => new ReportEntityModel()
+                //             {
+                //                 EntityName = namesList.FirstOrDefault(y => y.Key == x.Key).Value,
+                //                 EntityId = x.Key,
+                //                 TimePerTimeUnit = x.GroupBy(d => d.DateDone.Month).Select(g => g.Sum(s => s.TimeInMinutes)).ToList(),
+                //                 TotalTime = x.Sum(z => z.TimeInMinutes)
+                //             })
+                //             .ToList();
+                //         break;
+                //     case 2:
+                //         reportEntitiesList = jobsList.GroupBy(x => x.SiteId)
+                //             .Select(x => new ReportEntityModel()
+                //             {
+                //                 EntityName = namesList.FirstOrDefault(y => y.Key == x.Key).Value,
+                //                 EntityId = x.Key,
+                //                 TimePerTimeUnit = x.GroupBy(d => dfi.Calendar.GetWeekOfYear(d.DateDone, CalendarWeekRule.FirstDay, DayOfWeek.Monday)).Select(g => g.Sum(s => s.TimeInMinutes)).ToList(),
+                //                 TotalTime = x.Sum(z => z.TimeInMinutes)
+                //             })
+                //             .ToList();
+                //         break;
+                //     case 3:
+                //         reportEntitiesList = jobsList.GroupBy(x => x.SiteId)
+                //             .Select(x => new ReportEntityModel()
+                //             {
+                //                 EntityName = namesList.FirstOrDefault(y => y.Key == x.Key).Value,
+                //                 EntityId = x.Key,
+                //                 TimePerTimeUnit = x.GroupBy(d => d.DateDone.Day).Select(g => g.Sum(s => s.TimeInMinutes)).ToList(),
+                //                 TotalTime = x.Sum(z => z.TimeInMinutes)
+                //             })
+                //             .ToList();
+                //         break;
+                // }
+                // 
+                // var sumByTimeUnit = new List<int>();
+                // 
+                // int i;
+                // foreach (var reportEntity in reportEntitiesList)
+                // {
+                //     i = 0;
+                //     foreach (var timePerTimeUnit in reportEntity.TimePerTimeUnit)
+                //     {
+                //         if (sumByTimeUnit.Count <= i)
+                //         {
+                //             sumByTimeUnit.Add(timePerTimeUnit);
+                //         }
+                //         else
+                //         {
+                //             sumByTimeUnit[i] += timePerTimeUnit;
+                //         }
+                //         i++;
+                //     }
+                // }
+                // 
+                // var finalModel = new ReportModel()
+                // {
+                //     Entities = reportEntitiesList,
+                //     ReportHeaders = new List<ReportEntityHeaderModel>(),
+                //     TotalTime = reportEntitiesList.Sum(x => x.TotalTime),
+                //     TotalTimePerTimeUnit = sumByTimeUnit
+                // };
+                // 
+                // Console.WriteLine("Hello World!");
                 return new OperationDataResult<ReportModel>(true, new ReportModel());
             }
             catch (Exception e)
