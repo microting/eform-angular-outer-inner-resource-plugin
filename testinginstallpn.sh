035dac6685900f72d44613d1f8d28442b3f99463
René Schultz Madsen <rm@microting.dk>
2019-04-11 11:56:23 +0200

    Trying with a new plugininstaller.

--- testinginstallpn.sh
+++ testinginstallpn.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-sed '/\/\/ INSERT ROUTES HERE/i {' src/app/plugins/plugins.routing.ts -i
-sed '/\/\/ INSERT ROUTES HERE/i path: "machine-area-pn",' src/app/plugins/plugins.routing.ts -i
-sed '/\/\/ INSERT ROUTES HERE/i canActivate: [AuthGuard],' src/app/plugins/plugins.routing.ts -i
-sed '/\/\/ INSERT ROUTES HERE/i loadChildren: "./modules/machine-area-pn/machine-area-pn.module#MachineAreaPnModule"' src/app/plugins/plugins.routing.ts -i
-sed '/\/\/ INSERT ROUTES HERE/i }' src/app/plugins/plugins.routing.ts -i
+if [ ! -d "/var/www/microting/" ]; then
+	export current_folder=`pwd`
+	cd /var/www/microting
+	export plugin_count=`ls -lah | grep angular | grep plugin | wc -l`
+	cd $current_folder
+else
+	export plugin_count=0
+fi
+if (( $plugin_count > 1 )); then
+	sed '/\/\/ INSERT ROUTES HERE/i ,{' src/app/plugins/plugins.routing.ts -i
+	sed '/\/\/ INSERT ROUTES HERE/i path: "machine-area-pn",' src/app/plugins/plugins.routing.ts -i
+	sed '/\/\/ INSERT ROUTES HERE/i canActivate: [AuthGuard],' src/app/plugins/plugins.routing.ts -i
+	sed '/\/\/ INSERT ROUTES HERE/i loadChildren: "./modules/machine-area-pn/machine-area-pn.module#MachineAreaPnModule"' src/app/plugins/plugins.routing.ts -i
+	sed '/\/\/ INSERT ROUTES HERE/i }' src/app/plugins/plugins.routing.ts -i	
+
+else
+	sed '/\/\/ INSERT ROUTES HERE/i {' src/app/plugins/plugins.routing.ts -i
+	sed '/\/\/ INSERT ROUTES HERE/i path: "machine-area-pn",' src/app/plugins/plugins.routing.ts -i
+	sed '/\/\/ INSERT ROUTES HERE/i canActivate: [AuthGuard],' src/app/plugins/plugins.routing.ts -i
+	sed '/\/\/ INSERT ROUTES HERE/i loadChildren: "./modules/machine-area-pn/machine-area-pn.module#MachineAreaPnModule"' src/app/plugins/plugins.routing.ts -i
+	sed '/\/\/ INSERT ROUTES HERE/i }' src/app/plugins/plugins.routing.ts -i	
+fi
 
