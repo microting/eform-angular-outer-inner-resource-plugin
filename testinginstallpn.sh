a04d19cb1792e035953e8dbef973c487ecab4b57
René Schultz Madsen <rm@microting.dk>
2019-04-11 11:58:59 +0200

    Moving the statemenst around.

--- testinginstallpn.sh
+++ testinginstallpn.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 if [ ! -d "/var/www/microting/" ]; then
+	export plugin_count=0
+else
 	export current_folder=`pwd`
 	cd /var/www/microting
 	export plugin_count=`ls -lah | grep angular | grep plugin | wc -l`
 	cd $current_folder
-else
-	export plugin_count=0
 fi
 if (( $plugin_count > 1 )); then
 	sed '/\/\/ INSERT ROUTES HERE/i ,{' src/app/plugins/plugins.routing.ts -i
