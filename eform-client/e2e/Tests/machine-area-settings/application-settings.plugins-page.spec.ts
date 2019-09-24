16d171230c13f94301cd1bf3b5c7cdbfb1b20fa9
René Schultz Madsen <rm@microting.dk>
2019-09-24 07:55:16 +0200

    Doing a bit of cleanup. Trying to fix pause issues.

--- eform-client/e2e/Tests/machine-area-settings/application-settings.plugins-page.spec.ts
+++ eform-client/e2e/Tests/machine-area-settings/application-settings.plugins-page.spec.ts
@@ -10,30 +10,30 @@ describe('Application settings page - site header section', function () {
         loginPage.open('/auth');
     });
     it('should go to plugin settings page', function () {
-       loginPage.login();
-       myEformsPage.Navbar.advancedDropdown();
-       myEformsPage.Navbar.clickonSubMenuItem('Plugins');
-       browser.pause(8000);
-      browser.waitForVisible('#plugin-id', 80000);
+        loginPage.login();
+        myEformsPage.Navbar.advancedDropdown();
+        myEformsPage.Navbar.clickonSubMenuItem('Plugins');
+        browser.pause(40000);
+        // browser.waitForVisible('#plugin-id', 80000);
 
-      const plugin = pluginsPage.getFirstPluginRowObj();
-      expect(plugin.id).equal(1);
-      expect(plugin.name).equal('Microting Machine Area plugin');
-      expect(plugin.version).equal('1.0.0.0');
-      expect(plugin.status).equal('Deaktiveret');
-       // expect()
+        const plugin = pluginsPage.getFirstPluginRowObj();
+        expect(plugin.id).equal(1);
+        expect(plugin.name).equal('Microting Machine Area plugin');
+        expect(plugin.version).equal('1.0.0.0');
+        expect(plugin.status).equal('Deaktiveret');
+        // expect()
 
     });
 
     it('should activate the plugin', function () {
         pluginPage.pluginSettingsBtn.click();
-        browser.pause(8000);
+        browser.waitForVisible('#PluginDropDown', 40000);
         pluginPage.selectValue('PluginDropDown', 'PluginDropDown', 'Aktiveret');
-        browser.pause(8000);
         pluginPage.saveBtn.click();
-        browser.pause(40000);
+        browser.pause(2000);
         browser.refresh();
 
+        browser.pause(20000);
         const plugin = pluginsPage.getFirstPluginRowObj();
         expect(plugin.id).equal(1);
         expect(plugin.name).equal('Microting Machine Area plugin');
