ba690e08425802c1e15d0c173c69614839dbb509
René Schultz Madsen <rm@microting.dk>
2019-10-16 08:53:03 +0200

    Adding more changes.

--- testinginstallpn.sh
+++ testinginstallpn.sh
@@ -11,14 +11,14 @@ if (( $plugin_count > 1 )); then
 	sed '/\/\/ INSERT ROUTES HERE/i ,{' src/app/plugins/plugins.routing.ts -i
 	sed '/\/\/ INSERT ROUTES HERE/i path: "outer-inner-resource-pn",' src/app/plugins/plugins.routing.ts -i
 	sed '/\/\/ INSERT ROUTES HERE/i canActivate: [AuthGuard],' src/app/plugins/plugins.routing.ts -i
-	sed '/\/\/ INSERT ROUTES HERE/i loadChildren: "./modules/machine-area-pn/outer-inner-resource-pn.module#OuterInnerResourcePnModule"' src/app/plugins/plugins.routing.ts -i
+	sed '/\/\/ INSERT ROUTES HERE/i loadChildren: "./modules/outer-inner-resource-pn/outer-inner-resource-pn.module#OuterInnerResourcePnModule"' src/app/plugins/plugins.routing.ts -i
 	sed '/\/\/ INSERT ROUTES HERE/i }' src/app/plugins/plugins.routing.ts -i	
 
 else
 	sed '/\/\/ INSERT ROUTES HERE/i {' src/app/plugins/plugins.routing.ts -i
 	sed '/\/\/ INSERT ROUTES HERE/i path: "outer-inner-resource-pn",' src/app/plugins/plugins.routing.ts -i
 	sed '/\/\/ INSERT ROUTES HERE/i canActivate: [AuthGuard],' src/app/plugins/plugins.routing.ts -i
-	sed '/\/\/ INSERT ROUTES HERE/i loadChildren: "./modules/machine-area-pn/outer-inner-resource-pn.module#OuterInnerResourcePnModule"' src/app/plugins/plugins.routing.ts -i
+	sed '/\/\/ INSERT ROUTES HERE/i loadChildren: "./modules/outer-inner-resource-pn/outer-inner-resource-pn.module#OuterInnerResourcePnModule"' src/app/plugins/plugins.routing.ts -i
 	sed '/\/\/ INSERT ROUTES HERE/i }' src/app/plugins/plugins.routing.ts -i	
 fi
 
