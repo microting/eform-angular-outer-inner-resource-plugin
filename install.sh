605adc8e138ccaca6866d85312a0935aaa7c45da
René Schultz Madsen <rm@microting.dk>
2019-10-16 11:48:38 +0200

    Fixing the install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ fi
 
 cd /var/www/microting/eform-angular-outer-inner-resource-plugin
 su ubuntu -c \
-"dotnet restore eFormAPI/Plugins/MachineArea.Pn/MachineArea.Pn.sln"
+"dotnet restore eFormAPI/Plugins/OuterInnerResource.Pn/OuterInnerResource.Pn.sln"
 
 echo "################## START GITVERSION ##################"
 export GITVERSION=`git describe --abbrev=0 --tags | cut -d "v" -f 2`
@@ -17,23 +17,23 @@ echo "################## END GITVERSION ##################"
 su ubuntu -c \
 "dotnet publish eFormAPI/Plugins/MachineArea.Pn/MachineArea.Pn.sln -o out /p:Version=$GITVERSION --runtime linux-x64 --configuration Release"
 
-if [ -d "/var/www/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/machine-area-pn" ]; then
+if [ -d "/var/www/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/outer-inner-resource-pn" ]; then
 	su ubuntu -c \
-	"rm -fR /var/www/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/machine-area-pn"
+	"rm -fR /var/www/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/outer-inner-resource-pn"
 fi
 
 su ubuntu -c \
-"cp -av /var/www/microting/eform-angular-outer-inner-resource-plugin/eform-client/src/app/plugins/modules/machine-area-pn /var/www/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/machine-area-pn"
+"cp -av /var/www/microting/eform-angular-outer-inner-resource-plugin/eform-client/src/app/plugins/modules/outer-inner-resource-pn /var/www/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/outer-inner-resource-pn"
 su ubuntu -c \
 "mkdir -p /var/www/microting/eform-angular-frontend/eFormAPI/eFormAPI.Web/out/Plugins/"
 
-if [ -d "/var/www/microting/eform-angular-frontend/eFormAPI/eFormAPI.Web/out/Plugins/MachineArea" ]; then
+if [ -d "/var/www/microting/eform-angular-frontend/eFormAPI/eFormAPI.Web/out/Plugins/OuterInnerResource" ]; then
 	su ubuntu -c \
-	"rm -fR /var/www/microting/eform-angular-frontend/eFormAPI/eFormAPI.Web/out/Plugins/MachineArea"
+	"rm -fR /var/www/microting/eform-angular-frontend/eFormAPI/eFormAPI.Web/out/Plugins/OuterInnerResource"
 fi
 
 su ubuntu -c \
-"cp -av /var/www/microting/eform-angular-outer-inner-resource-plugin/eFormAPI/Plugins/MachineArea.Pn/MachineArea.Pn/out /var/www/microting/eform-angular-frontend/eFormAPI/eFormAPI.Web/out/Plugins/MachineArea"
+"cp -av /var/www/microting/eform-angular-outer-inner-resource-plugin/eFormAPI/Plugins/OuterInnerResource.Pn/OuterInnerResource.Pn/out /var/www/microting/eform-angular-frontend/eFormAPI/eFormAPI.Web/out/Plugins/OuterInnerResource"
 
 
 echo "Recompile angular"
