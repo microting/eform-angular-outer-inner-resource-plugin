b8d3d3481ace325a7e932b1a24b5eeb6d03ac8bd
René Schultz Madsen <rm@microting.dk>
2019-10-16 10:06:50 +0200

    Adding changes to fix the get changes from dev env.

--- devgetchanges.sh
+++ devgetchanges.sh
@@ -2,14 +2,14 @@
 
 cd ~
 
-if [ -d "Documents/workspace/microting/eform-angular-outer-inner-resource-plugin/eform-client/src/app/plugins/modules/machine-area-pn" ]; then
-	rm -fR Documents/workspace/microting/eform-angular-outer-inner-resource-plugin/eform-client/src/app/plugins/modules/machine-area-pn
+if [ -d "Documents/workspace/microting/eform-angular-outer-inner-resource-plugin/eform-client/src/app/plugins/modules/outer-inner-resource-pn" ]; then
+	rm -fR Documents/workspace/microting/eform-angular-outer-inner-resource-plugin/eform-client/src/app/plugins/modules/outer-inner-resource-pn
 fi
 
-cp -av Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/machine-area-pn Documents/workspace/microting/eform-angular-outer-inner-resource-plugin/eform-client/src/app/plugins/modules/machine-area-pn
+cp -av Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/outer-inner-resource-pn Documents/workspace/microting/eform-angular-outer-inner-resource-plugin/eform-client/src/app/plugins/modules/outer-inner-resource-pn
 
-if [ -d "Documents/workspace/microting/eform-angular-outer-inner-resource-plugin/eFormAPI/Plugins/MachineArea.Pn" ]; then
-	rm -fR Documents/workspace/microting/eform-angular-outer-inner-resource-plugin/eFormAPI/Plugins/MachineArea.Pn
+if [ -d "Documents/workspace/microting/eform-angular-outer-inner-resource-plugin/eFormAPI/Plugins/OuterInnerResource.Pn" ]; then
+	rm -fR Documents/workspace/microting/eform-angular-outer-inner-resource-plugin/eFormAPI/Plugins/OuterInnerResource.Pn
 fi
 
-cp -av Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/MachineArea.Pn Documents/workspace/microting/eform-angular-outer-inner-resource-plugin/eFormAPI/Plugins/MachineArea.Pn
+cp -av Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/OuterInnerResource.Pn Documents/workspace/microting/eform-angular-outer-inner-resource-plugin/eFormAPI/Plugins/OuterInnerResource.Pn
