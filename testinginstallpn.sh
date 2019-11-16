f354109d5c508ab6c3454e1cc4c0789f14af735d
René Schultz Madsen <rm@microting.com>
2019-11-16 09:29:41 +0100

    Fixing the testinginstallpn.sh

--- testinginstallpn.sh
+++ testinginstallpn.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
-if [ ! -d "/var/www/microting/" ]; then
-	export plugin_count=0
-else
-	export current_folder=`pwd`
-	cd /var/www/microting
-	export plugin_count=`ls -lah | grep angular | grep plugin | wc -l`
-	cd $current_folder
-fi
-if (( $plugin_count > 1 )); then
-	sed '/\/\/ INSERT ROUTES HERE/i ,{' src/app/plugins/plugins.routing.ts -i
-	sed '/\/\/ INSERT ROUTES HERE/i path: "outer-inner-resource-pn",' src/app/plugins/plugins.routing.ts -i
-	sed '/\/\/ INSERT ROUTES HERE/i canActivate: [AuthGuard],' src/app/plugins/plugins.routing.ts -i
-	sed '/\/\/ INSERT ROUTES HERE/i loadChildren: "./modules/outer-inner-resource-pn/outer-inner-resource-pn.module#OuterInnerResourcePnModule"' src/app/plugins/plugins.routing.ts -i
-	sed '/\/\/ INSERT ROUTES HERE/i }' src/app/plugins/plugins.routing.ts -i	
-
-else
-	sed '/\/\/ INSERT ROUTES HERE/i {' src/app/plugins/plugins.routing.ts -i
-	sed '/\/\/ INSERT ROUTES HERE/i path: "outer-inner-resource-pn",' src/app/plugins/plugins.routing.ts -i
-	sed '/\/\/ INSERT ROUTES HERE/i canActivate: [AuthGuard],' src/app/plugins/plugins.routing.ts -i
-	sed '/\/\/ INSERT ROUTES HERE/i loadChildren: "./modules/outer-inner-resource-pn/outer-inner-resource-pn.module#OuterInnerResourcePnModule"' src/app/plugins/plugins.routing.ts -i
-	sed '/\/\/ INSERT ROUTES HERE/i }' src/app/plugins/plugins.routing.ts -i	
-fi
+sed '/\/\/ INSERT ROUTES HERE/i {' src/app/plugins/plugins.routing.ts -i
+sed '/\/\/ INSERT ROUTES HERE/i path: "outer-inner-resource-pn",' src/app/plugins/plugins.routing.ts -i
+sed '/\/\/ INSERT ROUTES HERE/i canActivate: [AuthGuard],' src/app/plugins/plugins.routing.ts -i
+sed '/\/\/ INSERT ROUTES HERE/i loadChildren: "./modules/outer-inner-resource-pn/outer-inner-resource-pn.module#OuterInnerResourcePnModule"' src/app/plugins/plugins.routing.ts -i
+sed '/\/\/ INSERT ROUTES HERE/i },' src/app/plugins/plugins.routing.ts -i	
 
