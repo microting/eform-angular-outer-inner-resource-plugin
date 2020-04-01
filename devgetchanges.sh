8dbe1242e92d69eb42bffbf28bb08073bc0ee099
René Schultz Madsen <rm@microting.dk>
2020-04-01 14:17:30 +0200

    Fixing more.

--- devgetchanges.sh
+++ devgetchanges.sh
@@ -17,7 +17,7 @@ rm -fR Documents/workspace/microting/eform-angular-outer-inner-resource-plugin/e
 rm -fR Documents/workspace/microting/eform-angular-outer-inner-resource-plugin/eform-client/wdio-headless-plugin-step2.conf.js
 
 # Test files cp
-cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/monitoring-settings Documents/workspace/microting/eform-angular-outer-inner-resource-plugin/eform-client/e2e/Tests/monitoring-settings
-cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/monitoring-general Documents/workspace/microting/eform-angular-outer-inner-resource-plugin/eform-client/e2e/Tests/monitoring-general
-cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/Monitoring Documents/workspace/microting/eform-angular-outer-inner-resource-plugin/eform-client/e2e/Page\ objects/Monitoring
+cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/outer-inner-resource-settings Documents/workspace/microting/eform-angular-outer-inner-resource-plugin/eform-client/e2e/Tests/outer-inner-resource-settings
+cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/outer-inner-resource-general Documents/workspace/microting/eform-angular-outer-inner-resource-plugin/eform-client/e2e/Tests/outer-inner-resource-general
+cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/outer-inner-resource Documents/workspace/microting/eform-angular-outer-inner-resource-plugin/eform-client/e2e/Page\ objects/outer-inner-resource
 cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.js Documents/workspace/microting/eform-angular-outer-inner-resource-plugin/eform-client/wdio-headless-plugin-step2.conf.js
