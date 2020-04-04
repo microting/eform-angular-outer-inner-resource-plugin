e587164e727e48669dd9abc4b2575093e67ef9a3
René Schultz Madsen <rm@microting.dk>
2020-04-04 12:00:04 +0200

    Fixing the broken call for /bin/bash.

--- testinginstallpn.sh
+++ testinginstallpn.sh
@@ -1,4 +1,4 @@
-bin/bash
+#!/bin/bash
 perl -pi -e '$_.="  },\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
 perl -pi -e '$_.="  loadChildren: '\''./modules/outer-inner-resource-pn/outer-inner-resource-pn.module#OuterInnerResourcePnModule'\''\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
 perl -pi -e '$_.="  canActivate: [AuthGuard],\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
