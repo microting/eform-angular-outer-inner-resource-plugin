46ff14f8b482173d20e3baf153ab300453bf758b
René Schultz Madsen <rm@microting.dk>
2020-04-14 20:21:01 +0200

    Fixing the module loading.

--- testinginstallpn.sh
+++ testinginstallpn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 perl -pi -e '$_.="  },\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
-perl -pi -e '$_.="  loadChildren: '\''./modules/outer-inner-resource-pn/outer-inner-resource-pn.module#OuterInnerResourcePnModule'\''\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
-perl -pi -e '$_.="  canActivate: [AuthGuard],\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
-perl -pi -e '$_.="  path: '\''outer-inner-resource-pn'\'',\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
+perl -pi -e '$_.="      .then(m => m.OuterInnerResourcePnModule)\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
+perl -pi -e '$_.="    loadChildren: () => import('\''./modules/outer-inner-resource-pn/outer-inner-resource-pn.module'\'')\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
+perl -pi -e '$_.="    path: '\''outer-inner-resource-pn'\'',\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
 perl -pi -e '$_.="  {\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
