a45f95e2a4dac9a7c81f6e990732667c91f00a98
Alex <sahok6264@gmail.com>
2021-04-15 22:18:25 +0300

    Change times for one test

--- eform-client/wdio-headless-plugin-step2.conf.js
+++ eform-client/wdio-headless-plugin-step2.conf.js
@@ -158,7 +158,7 @@ exports.config = {
         ui: 'bdd',
         // require: 'ts-node/register',
         compilers: ['tsconfig-paths/register'],
-        timeout: 60000
+        timeout: 180000
     },
     //
     // =====
